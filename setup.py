610340f17ef7470347941843e53cbfb080b74f64
Xu Shen <shenxu2011@gmail.com>
2020-11-26 10:05:06 -0500

    as

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ version_string = open(VERSION_FILE, 'rt').read()
 version_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
 match = re.search(version_regex, version_string, re.M)
 
-if match:
-    version = match.group(1)
-else:
-    raise RuntimeError(
-        'Unable to find the version in {0}'.format(VERSION_FILE))
+# if match:
+#     version = match.group(1)
+# else:
+#     raise RuntimeError(
+#         'Unable to find the version in {0}'.format(VERSION_FILE))
